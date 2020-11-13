d662d28ea52404c07c4866e0d761348f0c67411a
Larisse <larisse.lima2@gmail.com>
2020-11-12 22:01:08 -0300

    Adiciona projeto inicial

---
+++ src/components/Lista.jsx
@@ -0,0 +1,17 @@
+import mulheres from '../services/dados'
+
+const Lista = () => {
+    return (
+        <div>
+            {mulheres.map(mulheres =>
+                <div key={mulheres.id}>
+                    <img width={100} src={mulheres.imagem} alt="Foto de mulheres" />
+                    <h1>{mulheres.nome}</h1>
+                    <p>{mulheres.descricao}</p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default Lista
---
+++ src/services/dados.js
@@ -0,0 +1,28 @@
+const mulheres = [
+    {
+        id: 1,
+        nome: "Ana Luiza",
+        descricao: "Site Reliability Analyst at @parentsIN & Front-end Development Student at @reprograma",
+        imagem: "https://avatars2.githubusercontent.com/u/60146477?s=460&u=15f058c58c330a547f8b08fdc8d7308584f790f3&v=4"
+    },
+    {
+        id: 2,
+        nome: "Lohany Formiga",
+        descricao: "UI & UX Designer. Apaixonada por Tecnologia e aluna no reprograma - Desenvolvedora Front-End",
+        imagem: "https://avatars0.githubusercontent.com/u/68750366?s=460&u=a91e92a0973b0208fe1b947506f2f496638484de&v=4"
+    },
+    {
+        id: 3,
+        nome: "Mariana Seidel",
+        descricao: "Estudante Front-end {reprograma}",
+        imagem: "https://avatars2.githubusercontent.com/u/70062295?s=460&u=e42d665dcd5cca07e1bc5ce2b09559dedc3ec53b&v=4"
+    },
+    {
+        id: 4,
+        nome: "Hellen Barreto",
+        descricao: "Aprendiz de Front-End no {REPROGRAMA}",
+        imagem: "https://avatars0.githubusercontent.com/u/70177308?s=460&u=d18618b06a448987c87145091cc2981110ec7d40&v=4"
+    }
+]
+
+export default mulheres
